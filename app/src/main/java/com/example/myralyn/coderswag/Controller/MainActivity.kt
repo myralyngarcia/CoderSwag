406027b23b8ee51e494d63b44a8a2bc7d7957a30
Myralyn Garcia <mygarcia@bbmtek.com>
2018-01-18 13:36:54 -0500

    category_list_item_layout

--- app/src/main/java/com/example/myralyn/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/myralyn/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.myralyn.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.myralyn.coderswag.Model.Category
 import com.example.myralyn.coderswag.R
+import com.example.myralyn.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //tell ArrayAdapter which context it will use, the format, the datasource
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,DataService.categories)
+        //tell listview what it needs to listen to
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/myralyn/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/myralyn/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.myralyn.coderswag.Model
 /**
  * Created by myralyn on 18/01/18.
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
