a4d352c7f4772d78950b511e858d87b96478975c
Myralyn Garcia <mygarcia@bbmtek.com>
2018-01-19 09:07:54 -0500

    added recyclerview dependencies

--- app/src/main/java/com/example/myralyn/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/myralyn/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myralyn.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.myralyn.coderswag.Adapters.CategoryAdapter
 import com.example.myralyn.coderswag.Model.Category
 import com.example.myralyn.coderswag.R
@@ -17,7 +18,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         adapter = CategoryAdapter(this, DataService.categories)
-        //tell listview what it needs to listen to
+        //tell ui element categoryListView what it needs to listen to
         categoryListView.adapter = adapter
+
+        //when we click on one of the item in listview it does not do anything
+        //do the following so it is clickable: Add onclick listener for the list view
+        //but below wont work for recycler view so we will comment it for now
+//        categoryListView.setOnItemClickListener { parent, view, position, id ->
+//            val category = DataService.categories[position]
+//            Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
+        }
     }
 }
